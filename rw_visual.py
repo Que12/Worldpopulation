714b2a2a97fd027b3e91fbf47482a45aa3463bc3
Que12 <rafal.krajewski03@onet.pl>
2020-12-07 17:58:44 +0100

    code aesthetics

--- rw_visual.py
+++ rw_visual.py
@@ -5,18 +5,17 @@ while True:
     rw = RandomWalk(50000)
     rw.fill_walk()
 
-    #określenie wielkości okna wykresu
-    plt.figure(dpi=128, figsize=(10, 6))  # figrzsize podaje w calach wielkosc obrazu 
+
+    plt.figure(dpi=128, figsize=(10, 6))  
 
 
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolor = 'none', s = 1)
-    #podkreslenie pierwszego i ostatniego punktu błądzenia losowego
-    plt.scatter(0,0, c = 'green', edgecolor = 'none', s = 100) # wyswietlenie punktu początkowego
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'yellow', edgecolor = 'none', s = 100) # wyswietlenie punktu ostatniego 
+
+    plt.scatter(0,0, c = 'green', edgecolor = 'none', s = 100) 
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'yellow', edgecolor = 'none', s = 100)  
     
-    #ukrywanie osi 
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
 
--- world_population.py
+++ world_population.py
@@ -3,14 +3,14 @@ from pygal.maps.world import World
 from pygal.style import RotateStyle, LightColorizedStyle
 from country_codes import get_country_code
 
-#wczytanie danych i umieszczenie ich na liście 
+
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-#utworzenie słownika danych dotyczące populacji
+
 cc_populations = {}
-# wyświetlenie populacji poszczegółnych państw w 2010 
+
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -19,7 +19,7 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
    
-# Podzielenie państw na try grupy wełdug liczebności populacji
+
 cc_pops_1,cc_pops_2, cc_pops_3 = {}, {}, {} 
 for cc, pop in cc_populations.items():
     if pop < 10000000:
@@ -29,7 +29,7 @@ for cc, pop in cc_populations.items():
     else:
         cc_pops_3[cc] = pop
 
-wm_style = RotateStyle('#336699', base_style = LightColorizedStyle ) # pobiera kolor rgb w systemie szesnastkowym
+wm_style = RotateStyle('#336699', base_style = LightColorizedStyle )
 wm = World(style = wm_style)
 wm.force_uri_protocol = 'http'
 wm.title = 'Populacja na świecie w 2010 roku (dane dla poszczególnych państw)'
